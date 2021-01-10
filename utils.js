b3685990c028d5b68834507436226ed6319b2fb3
fiatjaf <fiatjaf@alhur.es>
2021-01-10 19:15:06 -0300

    change an import, but this is meaningless.

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-import secp256k1 from 'noble-secp256k1'
+import * as secp256k1 from 'noble-secp256k1'
 
 export const makeRandom32 = () => secp256k1.utils.randomPrivateKey()
 export const sha256 = m => secp256k1.utils.sha256(Uint8Array.from(m))
