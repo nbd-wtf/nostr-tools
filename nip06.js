a46568d55c11001efacec937dda52fc64dd10961
fiatjaf <fiatjaf@gmail.com>
2021-12-31 23:09:43 -0300

    fix argument to micro-bip32

--- nip06.js
+++ nip06.js
@@ -8,7 +8,9 @@ import {HDKey} from 'micro-bip32'
 
 export function privateKeyFromSeed(seed) {
   let root = HDKey.fromMasterSeed(Buffer.from(seed, 'hex'))
-  return root.derive(`m/44'/1237'/0'/0'`).privateKey.toString('hex')
+  return Buffer.from(root.derive(`m/44'/1237'/0'/0'`).privateKey).toString(
+    'hex'
+  )
 }
 
 export function seedFromWords(mnemonic) {
