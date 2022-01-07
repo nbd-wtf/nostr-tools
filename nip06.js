e3631ba806e541dafece8009f4c55a68c47fdcab
fiatjaf <fiatjaf@gmail.com>
2022-01-06 21:46:17 -0300

    fix and update nip06.

--- nip06.js
+++ nip06.js
@@ -8,13 +8,13 @@ import {HDKey} from 'micro-bip32'
 
 export function privateKeyFromSeed(seed) {
   let root = HDKey.fromMasterSeed(Buffer.from(seed, 'hex'))
-  return Buffer.from(root.derive(`m/44'/1237'/0'/0'`).privateKey).toString(
+  return Buffer.from(root.derive(`m/44'/1237'/0'/0/0`).privateKey).toString(
     'hex'
   )
 }
 
 export function seedFromWords(mnemonic) {
-  return Buffer.from(mnemonicToSeedSync(mnemonic, wordlist)).toString('hex')
+  return Buffer.from(mnemonicToSeedSync(mnemonic)).toString('hex')
 }
 
 export function generateSeedWords() {
