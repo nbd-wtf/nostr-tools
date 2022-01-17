4b666e421be0d032999de0cb3479638544ec465c
fiatjaf <fiatjaf@gmail.com>
2022-01-17 16:37:19 -0300

    update nip05 to well-known version.

--- nip05.js
+++ nip05.js
@@ -1,52 +1,26 @@
-import {Buffer} from 'buffer'
-import dnsPacket from 'dns-packet'
+import fetch from 'cross-fetch'
 
-const dohProviders = [
-  'cloudflare-dns.com',
-  'fi.doh.dns.snopyta.org',
-  'basic.bravedns.com',
-  'hydra.plan9-ns1.com',
-  'doh.pl.ahadns.net',
-  'dns.flatuslifir.is',
-  'doh.dns.sb',
-  'doh.li'
-]
-
-let counter = 0
-
-export async function keyFromDomain(domain) {
-  let host = dohProviders[counter % dohProviders.length]
-
-  let buf = dnsPacket.encode({
-    type: 'query',
-    id: Math.floor(Math.random() * 65534),
-    flags: dnsPacket.RECURSION_DESIRED,
-    questions: [
-      {
-        type: 'TXT',
-        name: `_nostrkey.${domain}`
-      }
-    ]
-  })
-
-  let fetching = fetch(`https://${host}/dns-query`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/dns-message',
-      'Content-Length': Buffer.byteLength(buf)
-    },
-    body: buf
-  })
-
-  counter++
-
+export async function searchDomain(domain, query = '') {
   try {
-    let response = Buffer.from(await (await fetching).arrayBuffer())
-    let {answers} = dnsPacket.decode(response)
-    if (answers.length === 0) return null
-    return Buffer.from(answers[0].data[0]).toString()
-  } catch (err) {
-    console.log(`error querying DNS for ${domain} on ${host}`, err)
+    let res = await (
+      await fetch(`https://${domain}/.well-known/nostr.json?name=${query}`)
+    ).json()
+
+    return res.names
+  } catch (_) {
+    return []
+  }
+}
+
+export async function queryName(fullname) {
+  try {
+    let [name, domain] = fullname.split('@')
+    let res = await (
+      await fetch(`https://${domain}/.well-known/nostr.json?name=${name}`)
+    ).json()
+
+    return res.names && res.names[name]
+  } catch (_) {
     return null
   }
 }
