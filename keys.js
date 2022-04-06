c236e41f80e9e98d63ec9354282e99fc17cf8dea
monlovesmango <96307647+monlovesmango@users.noreply.github.com>
2022-04-06 16:05:42 -0500

    import 'Buffer'
    'Buffer' wasn't imported initially and was causing issues when I tried to use generatePrivateKey in a client I am building. not sure why Branle has no error, maybe I am doing something wrong?

--- keys.js
+++ keys.js
@@ -1,4 +1,5 @@
 import * as secp256k1 from '@noble/secp256k1'
+import {Buffer} from 'buffer'
 
 export function generatePrivateKey() {
   return Buffer.from(secp256k1.utils.randomPrivateKey()).toString('hex')
