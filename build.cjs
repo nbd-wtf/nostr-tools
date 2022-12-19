39aca167fb94d7a88343ecd0765676b676760cfa
fiatjaf <fiatjaf@gmail.com>
2022-12-19 15:46:31 -0300

    build for commonjs, esm and a standalone bundle.

--- build.cjs
+++ build.cjs
@@ -2,24 +2,35 @@
 
 const esbuild = require('esbuild')
 const alias = require('esbuild-plugin-alias')
-const nodeGlobals = require('@esbuild-plugins/node-globals-polyfill').default
 
-const buildOptions = {
-  entryPoints: ['index.js'],
-  outfile: 'nostr.js',
+let common = {
+  entryPoints: ['index.ts'],
   bundle: true,
-  format: 'esm',
   plugins: [
     alias({
       stream: require.resolve('readable-stream')
-    }),
-    nodeGlobals({buffer: true})
+    })
   ],
   define: {
     window: 'self',
     global: 'self'
   },
-  loader: {'.js': 'jsx'}
+  sourcemap: 'external'
 }
 
-esbuild.build(buildOptions).then(() => console.log('build success.'))
+esbuild
+  .build({...common, outdir: 'esm/', format: 'esm', packages: 'external'})
+  .then(() => console.log('esm build success.'))
+
+esbuild
+  .build({...common, outdir: 'cjs/', format: 'cjs', packages: 'external'})
+  .then(() => console.log('cjs build success.'))
+
+esbuild
+  .build({
+    ...common,
+    outdir: 'standalone/',
+    format: 'iife',
+    globalName: 'NostrTools'
+  })
+  .then(() => console.log('standalone build success.'))
