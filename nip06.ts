bb1e3f2fa6a32aac1b11e408b858752761e4008f
jaonoctus <jaonoctus@protonmail.com>
2023-01-17 05:17:53 -0300

    feat(nip06): add passphrase optional param

--- nip06.ts
+++ nip06.ts
@@ -7,8 +7,8 @@ import {
 } from '@scure/bip39'
 import {HDKey} from '@scure/bip32'
 
-export function privateKeyFromSeedWords(mnemonic: string): string {
-  let root = HDKey.fromMasterSeed(mnemonicToSeedSync(mnemonic))
+export function privateKeyFromSeedWords(mnemonic: string, passphrase?: string): string {
+  let root = HDKey.fromMasterSeed(mnemonicToSeedSync(mnemonic, passphrase))
   let privateKey = root.derive(`m/44'/1237'/0'/0/0`).privateKey
   if (!privateKey) throw new Error('could not derive private key')
   return secp256k1.utils.bytesToHex(privateKey)
