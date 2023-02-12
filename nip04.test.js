0edd8625df9f08f4fc4af022b47c81ce23eb1f5e
Moe Jangda <moe@tbd.email>
2023-02-12 02:43:27 -0600

    remove unneeded `require`
    Co-Authored-By: Trevor Pace <tmpace11@gmail.com>

--- nip04.test.js
+++ nip04.test.js
@@ -1,6 +1,5 @@
 /* eslint-env jest */
 
-globalThis.crypto = require('crypto')
 const {nip04, getPublicKey, generatePrivateKey} = require('./lib/nostr.cjs')
 
 test('encrypt and decrypt message', async () => {
