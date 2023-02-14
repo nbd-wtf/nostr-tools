54dfc7b9722d11a37a4551c0b5f07360b3c978d1
fiatjaf <fiatjaf@gmail.com>
2023-02-14 15:18:39 -0300

    validate that the event is an object.

--- event.ts
+++ event.ts
@@ -59,6 +59,7 @@ export function getEventHash(event: Event): string {
 }
 
 export function validateEvent(event: Event): boolean {
+  if (typeof event !== 'object') return false
   if (typeof event.content !== 'string') return false
   if (typeof event.created_at !== 'number') return false
   if (typeof event.pubkey !== 'string') return false
