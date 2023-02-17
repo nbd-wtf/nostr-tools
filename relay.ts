fbbe5da2c7decb50bfc8e320f501ce6f5b20e9d9
BilligsterUser <billigsteruser@protonmail.com>
2023-02-18 00:23:27 +0100

    allow alreadyHaveEvent fn to return boolean | Promise<boolean>
    Signed-off-by: BilligsterUser <billigsteruser@protonmail.com>

--- relay.ts
+++ relay.ts
@@ -32,7 +32,7 @@ export type Sub = {
 export type SubscriptionOptions = {
   id?: string
   skipVerification?: boolean
-  alreadyHaveEvent?: null | ((id: string, relay: string) => boolean)
+	alreadyHaveEvent?: null | ((id: string, relay: string) => boolean | Promise<boolean>)
 }
 
 export function relayInit(url: string): Relay {
@@ -89,7 +89,7 @@ export function relayInit(url: string): Relay {
         }
       }
 
-      function handleNext() {
+      async function handleNext() {
         if (incomingMessageQueue.length === 0) {
           clearInterval(handleNextInterval)
           handleNextInterval = null
@@ -105,7 +105,7 @@ export function relayInit(url: string): Relay {
           if (
             so &&
             so.alreadyHaveEvent &&
-            so.alreadyHaveEvent(getHex64(json, 'id'), url)
+            await so.alreadyHaveEvent(getHex64(json, 'id'), url)
           ) {
             return
           }
