82a2ab911e20e16f7dfd0d31eb64e3b603fbfeb5
0xc0de4c0ffee <43116881+0xc0de4c0ffee@users.noreply.github.com>
2023-02-25 18:15:24 +0545

    signInWithX function & more tests

--- nipxx.test.js
+++ nipxx.test.js
@@ -6,30 +6,61 @@ const {
 } = require('./lib/nostr.cjs')
 
 
-let username1 = 'me@example.com'
 let username2 = '0xc0de4c0ffee@0xc0de4c0ffee.eth.limo'
 let address = '0xc0De4c0FFEEC0dE4C0FfeEC0de4c0fFeec0de420' // address checksummed
-let caip10 = `eip155:1:${address}`
+let signer_info = `eip155:1:${address}`
 let password = 'horse staple battery'
-let sig1 = '0x'.padEnd(132, 'f')
-let sig2 = 'f'.padEnd(129, 'f')
-let privKey = 'ad94cd8ca2877102ad92d0fa3d2a918f4903dfb0445e33930e115f56deb733b9'
-
-test('private key from deterministic 0x+signature and identifiers', async () => {
-  expect(
-    await nipxx.privateKeyFromX(username1, caip10, sig1, password)
-  ).toEqual(privKey)
-})
+let sig = 'f'.padEnd(130, 'f')
 
 test('private key from deterministic signature and identifiers', async () => {
+  let username = 'me@example.com'
   expect(
-    await nipxx.privateKeyFromX(username1, caip10, sig1, password)
-  ).toEqual(privKey)
+    await nipxx.privateKeyFromX(username, signer_info, sig, password)
+  ).toEqual('ad94cd8ca2877102ad92d0fa3d2a918f4903dfb0445e33930e115f56deb733b9')
+
+  // without password
+  //expect(
+  //  await nipxx.privateKeyFromX(username, signer_info, sig)
+  //).toEqual('26ef9e1404ce02014e9bb503f6e99f5b9f2792722251856aaa3921222a98ef0e')
+
+  // 0x+hex signature
+  expect(
+    await nipxx.privateKeyFromX(username, signer_info, '0x' + sig, password)
+  ).toEqual('ad94cd8ca2877102ad92d0fa3d2a918f4903dfb0445e33930e115f56deb733b9')
 })
 
 
-test('login key from deterministic signature and identifiers', async () => {
+test('Login with deterministic signature and NIP02 identifiers', async () => {
   expect(
-    await nipxx.signInWithX(username2, caip10, sig1, password)
-  ).toEqual(privKey)
+    await nipxx.signInWithX('user123', signer_info, sig, password)
+  ).toEqual({
+    'petname': 'user123',
+    'privkey': 'e9257bd2c0d05af3533b580551d1964a04a43d53da1fc2167fa425f050e41012',
+    'profile': null
+  })
+})
+
+test('Invalid signature / password in NIP05 identifiers', async () => {
+  await expect(async () => {
+    await nipxx.signInWithX('0xc0de4c0ffee@0xc0de4c0ffee.eth.limo', signer_info, sig, password)
+  }).rejects.toThrow('Invalid Signature/Password');
+})
+
+test('NIP05 identifiers not set', async () => {
+  await expect(async () => {
+    await nipxx.signInWithX('zuck@cash.app', signer_info, sig, password)
+  }).rejects.toThrow('Nostr Profile Not Found');
+})
+
+test('Login with deterministic signature and NIP05 identifiers', async () => {
+  expect(
+    await nipxx.signInWithX('nipxxtest@0xc0de4c0ffee.eth.limo', signer_info, sig, password)
+  ).toEqual({
+    'petname': 'nipxxtest',
+    'profile': {
+      'pubkey': '2f88ee2dcc3c031533eca9f9185fab3b855e3213ac6861e1ece0ffab3cace470',
+      'relays': []
+    },
+    'privkey': '6a6a9cf580951d436fc30993d7480bf2a28be1ff3b80ae4236a5aaf378c87f0a'
+  })
 })
--- nipxx.ts
+++ nipxx.ts
@@ -1,88 +1,80 @@
 import * as secp256k1 from '@noble/secp256k1'
 
-import {
-  hkdf
-} from '@noble/hashes/hkdf'
-import {
-  sha256
-} from '@noble/hashes/sha256'
+import {hkdf} from '@noble/hashes/hkdf'
 
-import {
-  queryProfile
-} from './nip05'
-import {
-  getPublicKey
-} from './keys'
-import {
-  ProfilePointer
-} from './nip19'
+import {sha256} from '@noble/hashes/sha256'
 
+import {queryProfile} from './nip05'
+
+import {getPublicKey} from './keys'
+import {ProfilePointer} from './nip19'
+
+/**
+ * 
+ * @param username nip02/nip05 identifier
+ * @param caip10 CAIP identifier for the blockchain account
+ * @param sig Deterministic signature from X-wallet provider
+ * @param password Optional password
+ * @returns Deterministic private key as hex string
+ */
 export async function privateKeyFromX(
   username: string,
   caip10: string,
   sig: string,
-  password: string | undefined,
+  password: string | undefined
 ): Promise < string > {
-  if(sig.length < 64)
+  if (sig.length < 64)
     throw new Error("Signature too short");
-  let inputKey = await sha256(secp256k1.utils.hexToBytes(sig.toLowerCase().startsWith("0x")?sig.slice(2):sig))
+  let inputKey = await sha256(secp256k1.utils.hexToBytes(sig.toLowerCase().startsWith("0x") ? sig.slice(2) : sig))
   let info = `${caip10}:${username}`
   let salt = await sha256(`${info}:${password?password:""}:${sig.slice(-64)}`)
   let hashKey = await hkdf(sha256, inputKey, salt, info, 42)
   return secp256k1.utils.bytesToHex(secp256k1.utils.hashToPrivateKey(hashKey))
 }
 
-export async function signInWithX (
+export let registerWithX = privateKeyFromX // alias
+//export let signupWithX = privateKeyFromX // alias
+export let loginWithX = signInWithX // alias
+
+/**
+ * 
+ * @param username nip02/nip05 identifier
+ * @param caip10 CAIP identifier for the blockchain account
+ * @param sig Deterministic signature from X-wallet provider
+ * @param password Optional password
+ * @returns 
+ */
+export async function signInWithX(
   username: string,
-  password: string,
   caip10: string,
-  sig: string
+  sig: string,
+  password: string | undefined
 ): Promise < {
-  username: string,
+  petname: string,
   profile: ProfilePointer | null,
   privkey: string
-}> {
+} > {
   let profile = null
+  let petname = username
   if (username.includes(".")) {
-    profile = await queryProfile(username)
+    try {
+      profile = await queryProfile(username)
+    } catch (e) {
+      console.log(e)
+      throw new Error("Nostr Profile Not Found")
+    }
+    if(profile == null){
+      throw new Error("Nostr Profile Not Found")
+    } 
+    petname = (username.split("@").length == 2) ? username.split("@")[0] : username.split(".")[0]
   }
-  let pubkey = profile?.pubkey
-  let privkey = await privateKeyFromX(username, password, caip10, sig)
-  if (pubkey != getPublicKey(privkey)) {
-    //return null; //??
+  let privkey = await privateKeyFromX(username, caip10, sig, password)
+  let pubkey = getPublicKey(privkey)
+  if (profile?.pubkey && pubkey !== profile.pubkey) {
+    throw new Error("Invalid Signature/Password")
   }
-  // WIP
   return {
-    username,
-    profile,
-    privkey
-  }
-}
-
-export let loginWithX = signInWithX
-
-export async function registerWithX (
-  username: string,
-  password: string,
-  caip10: string,
-  sig: string
-): Promise < {
-  username: string,
-  profile: ProfilePointer | null,
-  privkey: string
-} | null > {
-  let profile = null
-  if (username.includes(".")) {
-    profile = await queryProfile(username)
-  }
-  let pubkey = profile?.pubkey
-  let privkey = await privateKeyFromX(username, password, caip10, sig)
-  if (pubkey != getPublicKey(privkey)) {
-    return null; //??
-  }
-
-  return {
-    username,
+    petname,
     profile,
     privkey
   }
