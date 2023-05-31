a467d5519e0d37745f469f7e732538e7640c8d26
Paul Miller <paul@paulmillr.com>
2023-05-31 14:40:18 +0000

    Format nip44

--- nip44.ts
+++ nip44.ts
@@ -2,27 +2,20 @@ import {randomBytes} from '@noble/hashes/utils'
 import {secp256k1} from '@noble/curves/secp256k1'
 import {base64} from '@scure/base'
 import {streamXOR as xchacha20_stream} from '@stablelib/xchacha20';
-
 import {utf8Decoder, utf8Encoder} from './utils.ts'
-import { sha256 } from '@noble/hashes/sha256';
+import {sha256} from '@noble/hashes/sha256';
 
-// @ts-ignore
-if (typeof crypto !== 'undefined' && !crypto.subtle && crypto.webcrypto) {
-  // @ts-ignore
-  crypto.subtle = crypto.webcrypto.subtle
-}
-
-function getConversationKey(privkeyA: string, pubkeyB: string) {
+export function getConversationKey(privkeyA: string, pubkeyB: string) {
   const key = secp256k1.getSharedSecret(privkeyA, '02' + pubkeyB)
   return sha256(key.slice(1, 33));
 }
 
-export async function encrypt(
+export function encrypt(
   privkey: string,
   pubkey: string,
   text: string,
   version = 1
-): Promise<string> {
+): string {
   if (version !== 1) throw new Error('NIP44: unknown encryption version');
   let key = getConversationKey(privkey, pubkey);
   let nonce = randomBytes(24);
@@ -33,11 +26,11 @@ export async function encrypt(
   return JSON.stringify({ciphertext: ctb64, nonce: nonceb64, v: 1})
 }
 
-export async function decrypt(
+export function decrypt(
   privkey: string,
   pubkey: string,
   data: string
-): Promise<string> {
+): string {
   let dt = JSON.parse(data);
   if (dt.v !== 1) throw new Error('NIP44: unknown encryption version');
   let {ciphertext, nonce} = dt;
