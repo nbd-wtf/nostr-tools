b1fc8ab401b8074f53e6a05a1a6a13422fb01b2d
Paul Miller <paul@paulmillr.com>
2023-06-29 01:03:37 +0000

    Switch nip44 library to noble

--- nip44.ts
+++ nip44.ts
@@ -1,7 +1,7 @@
 import {randomBytes} from '@noble/hashes/utils'
 import {secp256k1} from '@noble/curves/secp256k1'
 import {base64} from '@scure/base'
-import {streamXOR as xchacha20_stream} from '@stablelib/xchacha20'
+import {xchacha20} from '@noble/ciphers/chacha'
 import {utf8Decoder, utf8Encoder} from './utils.ts'
 import {sha256} from '@noble/hashes/sha256'
 
@@ -20,7 +20,7 @@ export function encrypt(
   let key = getConversationKey(privkey, pubkey)
   let nonce = randomBytes(24)
   let plaintext = utf8Encoder.encode(text)
-  let ciphertext = xchacha20_stream(key, nonce, plaintext, plaintext)
+  let ciphertext = xchacha20(key, nonce, plaintext, plaintext)
   let ctb64 = base64.encode(ciphertext)
   let nonceb64 = base64.encode(nonce)
   return JSON.stringify({ciphertext: ctb64, nonce: nonceb64, v: 1})
@@ -33,7 +33,7 @@ export function decrypt(privkey: string, pubkey: string, data: string): string {
   ciphertext = base64.decode(ciphertext)
   nonce = base64.decode(nonce)
   let key = getConversationKey(privkey, pubkey)
-  let plaintext = xchacha20_stream(key, nonce, ciphertext, ciphertext)
+  let plaintext = xchacha20(key, nonce, ciphertext, ciphertext)
   let text = utf8Decoder.decode(plaintext)
   return text
 }
