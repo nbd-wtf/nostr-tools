5f1ee4eea25258a1885b2fa4c5e3c3f6024d407a
Alex Gleason <alex@alexgleason.me>
2023-07-06 13:08:38 -0500

    Add NIP-30 module for custom emojis

---
+++ nip30.test.ts
@@ -0,0 +1,32 @@
+import {matchAll, replaceAll} from './nip30.ts'
+
+test('matchAll', () => {
+  const result = matchAll('Hello :blobcat: :disputed: ::joy:joy:')
+
+  expect([...result]).toEqual([
+    {
+      name: 'blobcat',
+      shortcode: ':blobcat:',
+      start: 6,
+      end: 15
+    },
+    {
+      name: 'disputed',
+      shortcode: ':disputed:',
+      start: 16,
+      end: 26
+    }
+  ])
+})
+
+test('replaceAll', () => {
+  const content = 'Hello :blobcat: :disputed: ::joy:joy:'
+
+  const result = replaceAll(content, ({name}) => {
+    return `<img src="https://ditto.pub/emoji/${name}.png" />`
+  })
+
+  expect(result).toEqual(
+    'Hello <img src="https://ditto.pub/emoji/blobcat.png" /> <img src="https://ditto.pub/emoji/disputed.png" /> ::joy:joy:',
+  )
+})
