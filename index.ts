5e51211bdc6bba152e37b23ef8fef82592744e9c
Jonathan Staab <shtaab@gmail.com>
2023-08-11 17:18:00 -0700

    Add support for nip44

--- index.ts
+++ index.ts
@@ -19,6 +19,7 @@ export * as nip27 from './nip27.ts'
 export * as nip28 from './nip28.ts'
 export * as nip39 from './nip39.ts'
 export * as nip42 from './nip42.ts'
+export * as nip44 from './nip44.ts'
 export * as nip57 from './nip57.ts'
 export * as nip98 from './nip98.ts'
 
---
+++ nip44.test.ts
@@ -0,0 +1,19 @@
+import crypto from 'node:crypto'
+
+import {encrypt, decrypt} from './nip44.ts'
+import {getPublicKey, generatePrivateKey} from './keys.ts'
+
+// @ts-ignore
+// eslint-disable-next-line no-undef
+globalThis.crypto = crypto
+
+test('encrypt and decrypt message', async () => {
+  let sk1 = generatePrivateKey()
+  let sk2 = generatePrivateKey()
+  let pk1 = getPublicKey(sk1)
+  let pk2 = getPublicKey(sk2)
+
+  expect(await decrypt(sk2, pk1, await encrypt(sk1, pk2, 'hello'))).toEqual(
+    'hello'
+  )
+})
