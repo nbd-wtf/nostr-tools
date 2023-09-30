084805b30d78fc01556b9332b5b57d6114db2971
Paul Miller <paul@paulmillr.com>
2023-09-30 18:36:29 +0000

    Update vectors

--- nip44.test.ts
+++ nip44.test.ts
@@ -39,7 +39,7 @@ test('NIP44: invalid_conversation_key', async () => {
   for (const v of vectors.invalid_conversation_key) {
     expect(() => {
       const key = utils.v2.getConversationKey(v.sec1, v.pub2)
-      const ciphertext = encrypt(key, v.plaintext)
+      const ciphertext = encrypt(key, 'a')
     }).toThrowError()
   }
 })
