a2a15567b7a015754a231f742c6169f36ad6405b
William Connatser <43946230+WilliamConnatser@users.noreply.github.com>
2023-10-23 20:19:47 -0500

    clean up test with a minor refactor to delete the ts-ignore

--- pool.test.ts
+++ pool.test.ts
@@ -104,12 +104,8 @@ test('list()', async () => {
   )
 
   // the actual received number will be greater than 2, but there will be no duplicates
-  expect(events.length).toEqual(
-    events
-      .map(evt => evt.id)
-      // @ts-ignore ???
-      .reduce((acc, n) => (acc.indexOf(n) !== -1 ? acc : [...acc, n]), []).length,
-  )
+  const uniqueEventCount = new Set(events.map(evt => evt.id)).size
+  expect(events.length).toEqual(uniqueEventCount)
 
   let relaysForAllEvents = events.map(event => pool.seenOn(event.id)).reduce((acc, n) => acc.concat(n), [])
   expect(relaysForAllEvents.length).toBeGreaterThanOrEqual(events.length)
