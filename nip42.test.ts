2ac69278ceb1954a5d5bd7ea4089b971e36b921c
fiatjaf <fiatjaf@gmail.com>
2023-12-16 11:21:49 -0300

    simplify nip-42.

--- nip42.test.ts
+++ nip42.test.ts
@@ -1,26 +1,14 @@
 import 'websocket-polyfill'
 
-import { finishEvent } from './event.ts'
-import { generatePrivateKey } from './keys.ts'
-import { authenticate } from './nip42.ts'
+import { makeAuthEvent } from './nip42.ts'
 import { relayInit } from './relay.ts'
 
 test('auth flow', () => {
-  const relay = relayInit('wss://nostr.kollider.xyz')
-  relay.connect()
-  const sk = generatePrivateKey()
+  const relay = relayInit('wss://nostr.wine')
 
-  return new Promise<void>(resolve => {
-    relay.on('auth', async challenge => {
-      await expect(
-        authenticate({
-          challenge,
-          relay,
-          sign: e => finishEvent(e, sk),
-        }),
-      ).rejects.toBeTruthy()
-      relay.close()
-      resolve()
-    })
-  })
+  const auth = makeAuthEvent(relay.url, 'chachacha')
+  expect(auth.tags).toHaveLength(2)
+  expect(auth.tags[0]).toEqual(['relay', 'wss://nostr.wine'])
+  expect(auth.tags[1]).toEqual(['challenge', 'chachacha'])
+  expect(auth.kind).toEqual(22242)
 })
--- nip42.ts
+++ nip42.ts
@@ -1,33 +1,17 @@
-import { type EventTemplate, type Event } from './event.ts'
+import { EventTemplate } from './event.ts'
 import { ClientAuth } from './kinds.ts'
-import { Relay } from './relay.ts'
 
 /**
- * Authenticate via NIP-42 flow.
- *
- * @example
- * const sign = window.nostr.signEvent
- * relay.on('auth', challenge =>
- *   authenticate({ relay, sign, challenge })
- * )
+ * creates an EventTemplate for an AUTH event to be signed.
  */
-export const authenticate = async ({
-  challenge,
-  relay,
-  sign,
-}: {
-  challenge: string
-  relay: Relay
-  sign: (e: EventTemplate) => Promise<Event> | Event
-}): Promise<void> => {
-  const evt: EventTemplate = {
+export function makeAuthEvent(relayURL: string, challenge: string): EventTemplate {
+  return {
     kind: ClientAuth,
     created_at: Math.floor(Date.now() / 1000),
     tags: [
-      ['relay', relay.url],
+      ['relay', relayURL],
       ['challenge', challenge],
     ],
     content: '',
   }
-  return relay.auth(await sign(evt))
 }
