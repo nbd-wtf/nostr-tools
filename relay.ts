daaa2ef0a13510cdeb9cd7a4ce03f2f417737068
fiatjaf <fiatjaf@gmail.com>
2023-12-21 19:59:12 -0300

    bring back relayConnect() as deprecated.

--- relay.ts
+++ relay.ts
@@ -1,6 +1,13 @@
 import { verifyEvent } from './pure.ts'
 import { AbstractRelay } from './abstract-relay.ts'
 
+/**
+ * @deprecated use Relay.connect() instead.
+ */
+export function relayConnect(url: string): Promise<Relay> {
+  return Relay.connect(url)
+}
+
 export class Relay extends AbstractRelay {
   constructor(url: string) {
     super(url, { verifyEvent })
