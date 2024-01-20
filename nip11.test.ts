c453bc5ec319d3ea3a6f80997a51663ec5fde5fb
Sepehr Safari <safari.sepehr@gmail.com>
2024-01-20 11:50:05 +0330

    revert nip11.test.ts with a todo flag

--- nip11.test.ts
+++ nip11.test.ts
@@ -2,13 +2,14 @@ import { describe, test, expect } from 'bun:test'
 import fetch from 'node-fetch'
 import { useFetchImplementation, fetchRelayInformation } from './nip11'
 
+// TODO: replace with a mock
 describe('requesting relay as for NIP11', () => {
   useFetchImplementation(fetch)
 
   test('testing a relay', async () => {
     const info = await fetchRelayInformation('wss://atlas.nostr.land')
     expect(info.name).toEqual('nostr.land')
-    expect(info.description).toEqual('nostr.land family of relays (fi-01 [leopard])')
+    expect(info.description).toEqual('nostr.land family of relays (us-or-01)')
     expect(info.fees).toBeTruthy()
     expect(info.supported_nips).toEqual([1, 2, 4, 9, 11, 12, 16, 20, 22, 28, 33, 40])
     expect(info.software).toEqual('custom')
