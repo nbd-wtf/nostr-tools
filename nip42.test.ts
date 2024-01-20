fbcfccda01aa02472634cf1d6c526a57b08d41a9
Sepehr Safari <safari.sepehr@gmail.com>
2024-01-20 11:31:18 +0330

    update nip42.test.ts with new mock relay class

--- nip42.test.ts
+++ nip42.test.ts
@@ -2,15 +2,15 @@ import { expect, test } from 'bun:test'
 
 import { makeAuthEvent } from './nip42.ts'
 import { Relay } from './relay.ts'
-import { newMockRelay } from './test-helpers.ts'
+import { MockRelay } from './test-helpers.ts'
 
 test('auth flow', async () => {
-  const { url } = newMockRelay()
-  const relay = await Relay.connect(url)
+  const mockRelay = new MockRelay()
+  const relay = await Relay.connect(mockRelay.getUrl())
   const auth = makeAuthEvent(relay.url, 'chachacha')
 
   expect(auth.tags).toHaveLength(2)
-  expect(auth.tags[0]).toEqual(['relay', url])
+  expect(auth.tags[0]).toEqual(['relay', mockRelay.getUrl()])
   expect(auth.tags[1]).toEqual(['challenge', 'chachacha'])
   expect(auth.kind).toEqual(22242)
 })
