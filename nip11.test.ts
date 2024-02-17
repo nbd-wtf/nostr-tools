280d483ef4b13897bcb55e12ae674c86f5387b4a
fiatjaf <fiatjaf@gmail.com>
2024-02-17 18:19:09 -0300

    adjust expected value in nip11 test.

--- nip11.test.ts
+++ nip11.test.ts
@@ -9,7 +9,7 @@ describe('requesting relay as for NIP11', () => {
   test('testing a relay', async () => {
     const info = await fetchRelayInformation('wss://atlas.nostr.land')
     expect(info.name).toEqual('nostr.land')
-    expect(info.description).toEqual('nostr.land family of relays (us-or-01)')
+    expect(info.description).toContain('nostr.land family')
     expect(info.fees).toBeTruthy()
     expect(info.supported_nips).toEqual([1, 2, 4, 9, 11, 12, 16, 20, 22, 28, 33, 40])
     expect(info.software).toEqual('custom')
