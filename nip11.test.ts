e30e08d8e2ed44c11092bf8b8a014e41b802624e
fiatjaf <fiatjaf@gmail.com>
2024-03-16 13:44:09 -0300

    update relay on nip11 test.

--- nip11.test.ts
+++ nip11.test.ts
@@ -7,11 +7,10 @@ describe('requesting relay as for NIP11', () => {
   useFetchImplementation(fetch)
 
   test('testing a relay', async () => {
-    const info = await fetchRelayInformation('wss://atlas.nostr.land')
-    expect(info.name).toEqual('nostr.land')
-    expect(info.description).toContain('nostr.land family')
-    expect(info.fees).toBeTruthy()
+    const info = await fetchRelayInformation('wss://nos.lol')
+    expect(info.name).toEqual('nos.lol')
+    expect(info.description).toContain('Generally accepts notes, except spammy ones.')
     expect(info.supported_nips).toEqual([1, 2, 4, 9, 11, 12, 16, 20, 22, 28, 33, 40])
-    expect(info.software).toEqual('custom')
+    expect(info.software).toEqual('git+https://github.com/hoytech/strfry.git')
   })
 })
