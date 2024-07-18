475a22a95f142815bb67f76a4e11e36754c97695
António Conselheiro <91137293+antonioconselheiro@users.noreply.github.com>
2024-07-18 13:33:29 -0300

    methods for abstract pool (#419)
    * include method to list current pool relays connections and to close all connections
    
    * fix prettier

--- abstract-pool.ts
+++ abstract-pool.ts
@@ -208,4 +208,16 @@ export class AbstractSimplePool {
       return r.publish(event)
     })
   }
+
+  listConnectionStatus(): Map<string, boolean> {
+    const map = new Map<string, boolean>()
+    this.relays.forEach((relay, url) => map.set(url, relay.connected))
+
+    return map
+  }
+
+  destroy(): void {
+    this.relays.forEach(conn => conn.close())
+    this.relays = new Map()
+  }
 }
