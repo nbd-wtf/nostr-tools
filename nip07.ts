c7f86ed226604d39df037c60894cc08186253551
Asai Toshiya <to.asai.60@gmail.com>
2025-02-22 00:26:30 +0900

    mark getRelays and get_relays as deprecated.

--- nip07.ts
+++ nip07.ts
@@ -4,6 +4,7 @@ import { RelayRecord } from './relay.ts'
 export interface WindowNostr {
   getPublicKey(): Promise<string>
   signEvent(event: EventTemplate): Promise<NostrEvent>
+  /** @deprecated removed from NIP */
   getRelays(): Promise<RelayRecord>
   nip04?: {
     encrypt(pubkey: string, plaintext: string): Promise<string>
