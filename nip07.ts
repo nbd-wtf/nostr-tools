174d36a4401a361c5c819f8b543b0b81a8947a28
fiatjaf <fiatjaf@gmail.com>
2025-03-01 19:44:33 -0300

    nip07: remove getRelays()

--- nip07.ts
+++ nip07.ts
@@ -1,11 +1,8 @@
 import { EventTemplate, NostrEvent } from './core.ts'
-import { RelayRecord } from './relay.ts'
 
 export interface WindowNostr {
   getPublicKey(): Promise<string>
   signEvent(event: EventTemplate): Promise<NostrEvent>
-  /** @deprecated removed from NIP */
-  getRelays(): Promise<RelayRecord>
   nip04?: {
     encrypt(pubkey: string, plaintext: string): Promise<string>
     decrypt(pubkey: string, ciphertext: string): Promise<string>
