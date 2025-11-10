aa464781e49489ad771a3e6a6a69b3fbf935327b
max-gy <xamyg@proton.me>
2025-11-10 13:29:55 +0100

    fix prettier checks on nip77 related *ts files

--- nip77.ts
+++ nip77.ts
@@ -1,17 +1,33 @@
 import { Filter } from './filter.ts'
 import { AbstractRelay } from './relay.ts'
 
-function sendNegentropyToRelay(open: boolean, relay: AbstractRelay, msg: string, filters: Filter[], subscriptionId?: any): void {
+function sendNegentropyToRelay(
+  open: boolean,
+  relay: AbstractRelay,
+  msg: string,
+  filters: Filter[],
+  subscriptionId?: any,
+): void {
   const subId = subscriptionId || Math.random().toString(36).slice(2, 10)
   const parts: any[] = [open ? 'NEG-OPEN' : 'NEG-MSG', subId, ...filters, msg]
   relay.send(JSON.stringify(parts))
 }
 
-export function sendNegentropyMessage(relay: AbstractRelay, msg: string, filters: Filter[], subscriptionId?: any): void {
+export function sendNegentropyMessage(
+  relay: AbstractRelay,
+  msg: string,
+  filters: Filter[],
+  subscriptionId?: any,
+): void {
   sendNegentropyToRelay(false, relay, msg, filters, subscriptionId)
 }
 
-export function openNegentropyWithMessage(relay: AbstractRelay, msg: string, filters: Filter[], subscriptionId?: any): void {
+export function openNegentropyWithMessage(
+  relay: AbstractRelay,
+  msg: string,
+  filters: Filter[],
+  subscriptionId?: any,
+): void {
   sendNegentropyToRelay(true, relay, msg, filters, subscriptionId)
 }
 
