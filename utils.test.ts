9db705d86c0798eb945e97a0d1c45326d5da680d
fiatjaf <fiatjaf@gmail.com>
2026-02-02 17:01:59 -0300

    delete queue test since we don't have queues anymore.

--- utils.test.ts
+++ utils.test.ts
@@ -1,7 +1,6 @@
 import { describe, test, expect } from 'bun:test'
 import { buildEvent } from './test-helpers.ts'
 import {
-  Queue,
   insertEventIntoAscendingList,
   insertEventIntoDescendingList,
   binarySearch,
@@ -218,48 +217,6 @@ describe('inserting into a asc sorted list of events', () => {
       }),
     )
     expect(list1).toHaveLength(3)
-  })
-})
-
-describe('enqueue a message into MessageQueue', () => {
-  test('enqueue into an empty queue', () => {
-    const queue = new Queue()
-    queue.enqueue('node1')
-    expect(queue.first!.value).toBe('node1')
-  })
-  test('enqueue into a non-empty queue', () => {
-    const queue = new Queue()
-    queue.enqueue('node1')
-    queue.enqueue('node3')
-    queue.enqueue('node2')
-    expect(queue.first!.value).toBe('node1')
-    expect(queue.last!.value).toBe('node2')
-  })
-  test('dequeue from an empty queue', () => {
-    const queue = new Queue()
-    const item1 = queue.dequeue()
-    expect(item1).toBe(null)
-  })
-  test('dequeue from a non-empty queue', () => {
-    const queue = new Queue()
-    queue.enqueue('node1')
-    queue.enqueue('node3')
-    queue.enqueue('node2')
-    const item1 = queue.dequeue()
-    expect(item1).toBe('node1')
-    const item2 = queue.dequeue()
-    expect(item2).toBe('node3')
-  })
-  test('dequeue more than in queue', () => {
-    const queue = new Queue()
-    queue.enqueue('node1')
-    queue.enqueue('node3')
-    const item1 = queue.dequeue()
-    expect(item1).toBe('node1')
-    const item2 = queue.dequeue()
-    expect(item2).toBe('node3')
-    const item3 = queue.dequeue()
-    expect(item3).toBe(null)
   })
 })
 
